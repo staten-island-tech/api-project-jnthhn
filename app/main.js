480131fb8f7e0aa8dd601d05be5d9cc998476703
jnthhn <jonathanh321@nycstudents.net>
2024-11-15 12:30:28 -0500

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+async function getData() {
+  //fetch returns a promise
+  try {
+    const response = await fetch("https://api.humorapi.com/jokes/random");
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("balls");
+  }
+}
+getData();
